e26ae6930ec4485b81858decfc89bf247ec8a21e
Trevor <trevoraangle@gmail.com>
2018-08-03 10:38:17 -0700

    Added tests for storage service

--- pocYT/src/app/storage.service.spec.ts
+++ pocYT/src/app/storage.service.spec.ts
@@ -10,30 +10,61 @@ fdescribe('StorageService', () => {
       providers: [StorageService]
     });
 
-  service = TestBed.get(StorageService);
+    service = TestBed.get(StorageService);
   });
 
-  describe('setAuthToken', () => {
-    
+  describe('setAuthToken:', () => {
+    it('asks sessionStorage to store given string', () => {
+      spyOn(sessionStorage, 'setItem');
+      service.setAuthToken('stub');
+      expect(sessionStorage.setItem).toHaveBeenCalledWith(jasmine.any(String), 'stub');
+    });
   });
 
-  describe('getAuthToken', () => {
+  describe('getAuthToken:', () => {
+    it('returns stored token if it exists', () => {
+      spyOn(sessionStorage, 'getItem').and.returnValue('stub');
+      let token = service.getAuthToken();
+      expect(token).toEqual('stub');
+      expect(sessionStorage.getItem).toHaveBeenCalled();
+    });
 
+    it('throws error if token does not exist', () => {
+      expect(service.getAuthToken).toThrowError();
+    });
   });
 
-  describe('deleteAuthToken', () => {
-    
+  describe('deleteAuthToken:', () => {
+    it('asks sessionStorage to remove token', () => {
+      spyOn(sessionStorage, 'removeItem');
+      service.deleteAuthToken();
+      expect(sessionStorage.removeItem).toHaveBeenCalled();
+    });
   });
 
-  describe('setChannelTitle', () => {
-
+  describe('setChannelTitle:', () => {
+    it('asks sessionStorage to store given string', () => {
+      spyOn(sessionStorage, 'setItem');
+      service.setChannelTitle('stub');
+      expect(sessionStorage.setItem).toHaveBeenCalledWith(jasmine.any(String), 'stub');
+    });
   });
 
-  describe('getChannelTitle', () => {
-
+  describe('getChannelTitle:', () => {
+    it('returns stored title', () => {
+      let spy = spyOn(sessionStorage, 'getItem').and.returnValue('stub');
+      expect(service.getChannelTitle()).toEqual('stub');
+      expect(spy).toHaveBeenCalled();
+      spy.and.returnValue(null);
+      expect(service.getChannelTitle()).toEqual(null);
+    });
   });
 
-  describe('deleteChannelTitle', () => {
-
+  describe('deleteChannelTitle:', () => {
+    it('asks sessionStorage to remove title', () => {
+      spyOn(sessionStorage, 'removeItem');
+      service.deleteChannelTitle();
+      expect(sessionStorage.removeItem).toHaveBeenCalled();
+    });
   });
 });
